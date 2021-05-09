cd24f0eb08cac2046d631318aa24fc8c871f9284
danyal0726 <danyal0726@gmail.com>
2021-05-08 23:18:49 -0400

    Day 2: Changed & Customized Admin Page

--- django-review/POLLSTER_PROJECT/pollster/polls/admin.py
+++ django-review/POLLSTER_PROJECT/pollster/polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Question, Choice
 # Register your models here.
+
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster Admin Area"
+
+""" admin.site.register(Question)
+admin.site.register(Choice) """
+
+class ChoiceInline(admin.TabularInline): 
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin): 
+    fieldsets = [(None, {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
