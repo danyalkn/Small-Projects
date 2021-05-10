6f18add731287018249786349ff4e388eb313ac9
danyal0726 <danyal0726@gmail.com>
2021-05-09 23:09:42 -0400

    Day 3: Setting up Views

--- django-review/POLLSTER_PROJECT/pollster/polls/views.py
+++ django-review/POLLSTER_PROJECT/pollster/polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Question, Choice
+
+# Get questions and display them
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
